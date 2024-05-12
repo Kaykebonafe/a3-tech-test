865e30e61dab7ede4c97391ea1d6508918f0b024
Kayke B. Kakazu <kayke.bonafe98@gmail.com>
2024-05-12 19:13:05 -0300

    Removed unecessary prints.

--- a3_tech_test/app/controllers/chat.py
+++ a3_tech_test/app/controllers/chat.py
@@ -2,10 +2,7 @@ from a3_tech_test.app.modules.models.gemini_api import query_engine
 from a3_tech_test.app.interfaces.chat import chat_message
 
 async def achat(query: chat_message):
-    print("iniciando achar")
     response = await query_engine.aquery(query.query)  
-    
-    print("terminando achat")
     
     return {
         "response": response.response # .sources[0].content
